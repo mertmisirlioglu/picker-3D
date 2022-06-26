c80482b7c7cb9a5d3f13333d71f10be68f827f59
Mert Mısırlıoğlu <m.misirlioglu@azerion.com>
2022-06-27 01:58:00 +0300

    speed constant changed

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,7 +2,7 @@
     {
         public static int MIN_TARGET = 20;
 
-        public static float SPEED_MULTIPLICATOR = 0.01f;
+        public static float SPEED_MULTIPLICATOR = 0.04f;
         public static float START_MOVE_SPEED = 1.5f;
         public static float MAX_MOVE_SPEED = 3f;
 
